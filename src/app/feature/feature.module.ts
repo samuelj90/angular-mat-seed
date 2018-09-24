3d4b2613b4076e59ac2af932ef1f8736e43c59b8
Samuel J Mathew <samuelj90@gmail.com>
2018-09-24 18:36:13 +0530

    Adding default navigation to dashboard

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { FeatureRoutingModule } from './feature-routing.module';
 import {SharedModule} from '../shared/shared.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [
     SharedModule,
     FeatureRoutingModule
   ],
-  declarations: [DashboardComponent]
+  declarations: []
 })
 export class FeatureModule { }
