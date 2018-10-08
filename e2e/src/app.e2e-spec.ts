88303c3f1825aa8f9fdc1591f209ee09433dccae
Samuel J Mathew <samuelj90@gmail.com>
2018-10-08 19:37:13 +0530

    Updating E2E tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,6 +1,6 @@
-import {AppPage} from './app.po';
+import { AppPage } from './app.po';
 
-describe('workspace-project App', () => {
+describe('Angular Material Seed', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -10,5 +10,22 @@ describe('workspace-project App', () => {
   it('should display home component', () => {
     page.navigateTo();
     expect(page.getComponent('home')).toBeDefined();
+  });
+
+  it('should display login if user is not logged in', () => {
+    page.navigateTo();
+    expect(page.getComponent('login')).toBeDefined();
+  });
+
+  it('should display dashboard if user is logged in', () => {
+    page.navigateToLogin();
+    page.loginWithCredentials({username: 'admin', password: 'admin@123'});
+    expect(page.getCurrentUrl()).toContain('dashboard');
+  });
+
+  it('should display login if wrong user tries logged in', () => {
+    page.navigateToLogin();
+    page.loginWithCredentials({username: 'wrongadmin', password: 'wrongadmin@123'});
+    expect(page.getCurrentUrl()).toContain('login');
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,15 +1,30 @@
-import {browser, by, element} from 'protractor';
+import { browser, by, element, ExpectedConditions } from 'protractor';
 
 export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  navigateToLogin() {
+    return browser.get('/auth/login');
   }
 
   getComponent(name) {
     return element(by.css(name));
   }
+
+  getCurrentUrl() {
+    return browser.getCurrentUrl();
+  }
+
+  loginWithCredentials(credentias: { username: string, password: string }) {
+    const userNameInput = element(by.id('username'));
+    browser.wait(ExpectedConditions.presenceOf(userNameInput), 10000);
+    userNameInput.sendKeys(credentias.username);
+    const passwordInput = element(by.id('password'));
+    passwordInput.sendKeys(credentias.password);
+    const loginSubmitButton = element(by.css('button[type=submit]'));
+    loginSubmitButton.click();
+  }
+
 }
